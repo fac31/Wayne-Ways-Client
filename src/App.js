a604891ec038f425450e1b6702ddcbbc1d4cfe1f
OnlyOneLz <elliotrobertlewis@hotmail.com>
2024-05-21 11:34:55 +0100

    google maps api start

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Home from './components/home';
+import Auth from './components/auth';
 
 function App() {
     return (
         <div>
             <Routes>
                 <Route path="/" element={<Home />} />
+                <Route path="/authentication" element={<Auth />} />
             </Routes>
         </div>
     );
--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useEffect } from 'react';
+import getLocation from '../utilities/getLocation';
 
-const home = () => {
+const Home = () => {
+    useEffect(() => {
+        getLocation();
+    }, []);
+    const latitude = localStorage.getItem('latitude');
+    const longitude = localStorage.getItem('longitude');
+    console.log(latitude, longitude);
+
     return (
-        <div>Hello World</div>
-    )
-}
+        <div>
+            <gmp-map
+                center={`${latitude},${longitude}`}
+                zoom="15"
+                map-id="DEMO_MAP_ID"
+                style={{ height: 400, width: 800 }}
+            ></gmp-map>
+        </div>
+    );
+};
 
-export default home
+export default Home;
